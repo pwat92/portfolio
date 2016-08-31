76cfad13153bfce294aa751ee22ab49c69b7721d
pwat92 <patrick.watson92@gmail.com>
2016-08-31 12:08:33 -0400

    make setup rake task, setup beginning of testimonials

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,13 +8,11 @@ class StaticController < ApplicationController
     @email = "patrick.watson92@gmail.com"
     @snapchat_link = "https://www.snapchat.com/add/pwat92"
     @instagram_link = ""
-    @facebook_link = ""
+    @facebook_link = "https://www.facebook.com/patrick.watson.7587"
     @linkedin_link = "https://ca.linkedin.com/in/patrick-watson"
 
-    #skill variables
-    #@skills_per_row = 4
     @skills = Skill.all
-    #@number_of_rows = (@skills.count.to_f / @skills_per_row.to_f).ceil
-    #@index = 0
+
+    @about_me_blurb = ""
   end
 end
